747170e69615d47f7f28811551cb854fa5e47ab7
@H3LD3R <helderamorac@gmail.com>
2023-10-10 21:15:10 -0300

    new save

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+let a= [1,2,3];
+let b= [3,2,1];
+let A = 0;
+let B = 0;
+
+for (let i = 0; i < a.length; arrA++) {
+    if (a[0] > b[0]){
+        A  = 1;
+    } else if (a[1] <[1]){
+        B = 1;
+        console.log(A,B);
+    } return 0;
+}
